483554567ec9d7f43d6ad91366966c344a349649
JanSzalanski <jan@szalanski.eu>
2021-09-01 21:35:08 +0200

    added kontakt.php

---
+++ kontakt.php
@@ -0,0 +1,21 @@
+<?php
+
+ header('Location: index.php');
+
+?>  
+
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kontakt</title>
+</head>
+<body>
+
+
+
+    
+</body>
+</html>
