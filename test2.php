127c87733ac766d1dff1a3449ec278125a847a53
JanSzalanski <jan@szalanski.eu>
2021-09-02 14:30:23 +0200

    next try...

---
+++ test2.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start(); 
+
+header('Location: index.php'); #gitignore 
+
+define('SITE_KEY', '6Lepfl4bAAAAAZl0yxb7VBFTGP-ECA7-'); #gitignore 
+?>  
+
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test2v</title>
+</head>
+<body>
+
+
+
+    
+</body>
+</html>
